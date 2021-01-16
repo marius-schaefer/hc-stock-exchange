f805454ee5a3f3aaea41b472e93a930219518c1b
marius-schaefer <marius.schaefer2005@gmail.com>
2021-01-16 15:25:39 +0200

    Commented out an unessecary function as well as added commentss explaining what does what

--- overseer_utils.py
+++ overseer_utils.py
@@ -1,12 +1,15 @@
 import requests
 import json
 
-
+#Gets data from the Overseer Github
 url = 'https://github.com/KhushrajRathod/TheOverseerBackend/releases/download/Latest/results.json'
 x = requests.get(url)
 
+#Formates the data into a list containing lists
 json_data = json.loads(x.content)
 
+
+#Function for getting the message count of a user
 def find_user_message_count(user_id, json_data):
     ind = 0
     for item in json_data:
@@ -17,8 +20,9 @@ def find_user_message_count(user_id, json_data):
     return json_data[ind][1]
 
 
-def update_message_count(dictionary):
-    for item in dictionary:
-        new_message_count = find_user_message_count(item, json_data)
-        dictionary[item] = new_message_count
-    return dictionary
+# This function may be unneccesary and may be replaced by a db table
+#def update_message_count(dictionary):
+#    for item in dictionary:
+#       new_message_count = find_user_message_count(item, json_data)
+#       dictionary[item] = new_message_count
+#    return dictionary
