8be606fdd8ab6a39ee76242d5f84e106ce351306
marius-schaefer <marius.schaefer2005@gmail.com>
2021-02-13 11:48:47 +0200

    New Initial Commit, restarting

--- utils/stock_utils.py
+++
@@ -1,8 +0,0 @@
-from overseer_utils import *
-
-#stock price calculation function:
-def stock_price_calculate(user_id, amount_of_transactions):
-    message_amount = int(get_user_message_count(user_id))
-    stock_price = (((message_amount//100) * 50) + (amount_of_transactions * 50))// 100
-    return stock_price
-
