258cb16adda2228960ffd4cedaeeff30c21797e1
marius-schaefer <marius.schaefer2005@gmail.com>
2021-02-19 12:11:25 +0200

    Commit 3, Fixed an issue for creating stock-creator table and created the first lines of code for the slack bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from init_db import *
-import slack
+import os
+from slack_bolt import App
 
 
 #checks if all-stocks table exists in the db
@@ -21,7 +22,24 @@ if not trades_table:
     create_trades_table()
 
 #checks if stock-creator table exists in the db
-trades_table = check_for_table('stock-creator')
+stock_creator_table = check_for_table('stock-creator')
 #creates stock-creator table if the table does not exist
-if not trades_table:
-    create_stock_creator_table
+if not stock_creator_table:
+   create_stock_creator_table
+
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token='PUT BOT TOKEN HERE',
+    signing_secret='PUT SIGNING SECRET HERE'
+)
+
+# Listens to incoming messages that contain "hello"
+@app.message("!hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
