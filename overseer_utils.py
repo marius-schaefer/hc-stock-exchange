4e97fac8cd2f5f779d697e92e47a4b2273873f6f
marius-schaefer <marius.schaefer2005@gmail.com>
2021-02-24 21:01:08 +0200

    Commit 5, Removed update_stock_price() function

--- overseer_utils.py
+++ overseer_utils.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from db_utils import *
 
 
 #Fucntion that finds the message count for a certain user using the Overseer API
@@ -22,23 +21,4 @@ def find_user_message_count(slack_id):
     index -= 1
     return json_data[index][1]
 
-
-def update_stock_price(stock_creator):
-    #Getting the data that is needed
-    message_count = find_user_message_count(stock_creator)
-    trade_count = get_trade_count(stock_creator)
-
-    #Calculating the stock price and storing it in a variable:
-    stock_price = (((message_count/300)*50)+(trade_count*50))//100
-
-    #creates or connects to an existing db
-    conn = sqlite3.connect('hse.db')
-    #creates cursor
-    c = conn.cursor()
-
-    #Adds the data of the stock we want to add:
-    c.execute("UPDATE stock SET stock_price = ? WHERE stock_creator = ?", (stock_price, stock_creator)) 
-
-    conn.commit()
-    conn.close()
 
