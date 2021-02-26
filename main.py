9b32b010ef7fa20178ff74426116b90364bd7c79
marius-schaefer <marius.schaefer2005@gmail.com>
2021-02-26 22:30:48 +0200

    Commit 8, Created @app.view('buy_modal_3')

--- main.py
+++ main.py
@@ -2,31 +2,33 @@ from init_db import *
 import os
 from slack_bolt import App
 from slackbot_utils import *
+from hn_utils import *
+import time
 
 
 
 
 #checks if all-stocks table exists in the db
 all_stocks_table = check_for_table('all-stocks')
-creates all-stocks table if the table does not exist
+#creates all-stocks table if the table does not exist
 if not all_stocks_table:
     create_all_stocks_table()
 
 #checks if stock table exists in the db
 stock_table = check_for_table('stock')
-creates stock table if the table does not exist
+#creates stock table if the table does not exist
 if not stock_table:
     create_stock_table()
 
 #checks if trades table exists in the db
 trades_table = check_for_table('trades')
-creates trades table if the table does not exist
+#creates trades table if the table does not exist
 if not trades_table:
     create_trades_table()
 
 #checks if stock-creator table exists in the db
 stock_creator_table = check_for_table('stock-creator')
-creates stock-creator table if the table does not exist
+#creates stock-creator table if the table does not exist
 if not stock_creator_table:
    create_stock_creator_table
 
@@ -66,9 +68,42 @@ def update_to_buy_modal_2(ack, body, client, view):
 @app.view('buy_modal_2')
 def update_to_buy_modal_3(ack, body, client, view):
     stock_symbol_plus_amount = view['state']['values']['static_select']['amount-to-buy']
+    
     ack()
 
     buy_modal_3(ack, body, client, stock_symbol_plus_amount)
+
+
+@app.view('buy_modal_3')
+def handle_submitted_buy_modal_data(ack, body, client, view):
+    #Getting Data from the submitted modals
+    user=body["user"]["id"]
+    values=view['title']['text']
+
+    #Spliting values inorder to get the stock symbol and amount
+    split_values = values.split(' ')
+
+    #Setting amount and stock_symbol variables = to the data
+    amount = split_values[1]
+    stock_symbol = split_values[2]
+
+    #getting the price of the stock:
+    stock_price = get_stock_price(stock_symbol)
+    
+    #Calculating the amount the total price:
+    amount = int(amount)
+    total_price = stock_price * amount
+
+    #Create an invoice:
+    create_invoice(total_price, user)
+
+    #Sleep for 3 minutes and then check for a hn transaction
+    time.sleep(180)
+    if check_for_hn_transaction(user, total_price):
+        set_stock_owner_plus_payout(stock_symbol, user, amount)
+    else:
+        pass
+
 
 
 # Start your app
